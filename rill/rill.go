97780673687119ff712dac1849f18b2361f618cd
Fabien Lamarche-Filion <fabien.lf@samsung.com>
2019-05-31 16:12:29 -0400

    Add ingest and write for acc

--- rill/rill.go
+++ rill/rill.go
@@ -10,6 +10,16 @@ type Acc struct {
 	acc *C.struct_rill_acc
 }
 
+type Rows struct {
+	len C.size_t;
+	data *C.struct_rill_row
+}
+
+type Row struct {
+	a C.rill_val_t;
+	b C.rill_val_t
+}
+
 func AccOpen(path string, capacity int) Acc {
 	acc := C.rill_acc_open(C.CString(path), C.size_t(capacity))
 	return Acc{acc}
@@ -17,4 +27,22 @@ func AccOpen(path string, capacity int) Acc {
 
 func AccClose(acc Acc) {
 	C.rill_acc_close((*C.struct_rill_acc)(acc.acc))
+}
+
+func AccIngest(acc Acc, val_a int, val_b int) {
+	C.rill_acc_ingest(
+		(*C.struct_rill_acc)(acc.acc),
+		C.rill_val_t(val_a),
+		C.rill_val_t(val_b))
+}
+
+func AccWrite(acc Acc, file string, ts int) {
+	C.rill_acc_write(
+		(*C.struct_rill_acc)(acc.acc),
+		C.CString(file),
+		C.rill_ts_t(ts))
+}
+
+func Rotate(dir string, now_ts int, expire_time int) {
+	C.rill_rotate(C.CString(dir), C.rill_ts_t(now_ts), C.rill_ts_t(expire_time))
 }
--- rill/rill_test.go
+++ rill/rill_test.go
@@ -7,12 +7,26 @@ import (
 )
 
 func TestAccOpenClose(t *testing.T) {
-	os.RemoveAll("AccTest")
+	os.RemoveAll("test")
 	os.Mkdir("." + string(filepath.Separator) + "test",0777)
-	acc := AccOpen("test", 1000000)
+	acc := AccOpen("test", 1)
 	acc_file := filepath.Join("./test", "acc")
 	if _, err := os.Stat(acc_file) ; os.IsNotExist(err) {
 		t.Errorf("File %v not created by rill_acc_open", acc_file)
 	}
 	AccClose(acc)
 }
+
+func TestAccIngest(t *testing.T) {
+	os.RemoveAll("test")
+	os.Mkdir("." + string(filepath.Separator) + "test",0777)
+	acc := AccOpen("test", 1000000)
+	AccIngest(acc, 1, 1)
+	AccWrite(acc, "test/thing", 1)
+}
+
+// func TestRotate(t *testing.T) {
+// 	os.RemoveAll("test")
+// 	os.Mkdir("." + string(filepath.Separator) + "test",0777)
+// 	acc := AccOpen("test", 1000000)
+// }
